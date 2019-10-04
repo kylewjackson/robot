1e21c196416dd2de32e83592df100e995c9fa462
Kyle Jackson <kylewalkerjackson@gmail.com>
2019-10-03 22:22:59 -0500

    change to directional/accelerate

--- robot-app.nosync/src/App.js
+++ robot-app.nosync/src/App.js
@@ -6,29 +6,43 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    //up/right is position, down/left is negative
     this.state = {
       row: 3,
       col: 3,
+      direction: [0, 1],
     };
 
     this.handleMove = this.handleMove.bind(this);
+    this.handleTurn = this.handleTurn.bind(this);
   };
 
-  handleMove(direction) {
-    const rowPosition =
-      parseInt(this.state.row) + parseInt(direction[0]) < 1 ?
-        1 : parseInt(this.state.row) + parseInt(direction[0]) > count ?
-        count : parseInt(this.state.row) + parseInt(direction[0]);
-    const colPosition =
-      parseInt(this.state.col) + parseInt(direction[1]) < 1 ?
-        1 : parseInt(this.state.col) + parseInt(direction[1]) > count ?
-        count : parseInt(this.state.col) + parseInt(direction[1]);
-    this.setState({row: rowPosition, col: colPosition});
+  handleTurn(dir) {
+    this.setState({direction: dir});
   };
+
+  handleMove() {
+    if (parseInt(this.state.direction[0]) !== 0) {
+      const nextRow = parseInt(this.state.direction[0]) + parseInt(this.state.row);
+      nextRow < 1 || nextRow > count ? this.bump() : this.setState({row: nextRow});
+    } else {
+      const nextCol = parseInt(this.state.direction[1]) + parseInt(this.state.col);
+      nextCol < 1 || nextCol > count ? this.bump() : this.setState({col: nextCol});
+    };
+  };
+
+    bump() {
+      alert("You've hit a wall!");
+    };
 
   render() {
     const rows = Array(count).fill(null).map((item, i) =>
-      <Row key={`row-${i + 1}`} num={i + 1} row={this.state.row === i + 1 ? this.state.row : false} col={this.state.col} />
+      <Row
+        key={`row-${i + 1}`}
+        num={i + 1}
+        row={this.state.row === i + 1 ? this.state.row : false}
+        col={this.state.col}
+      />
     );
     return (
       <>
@@ -36,7 +50,7 @@ class App extends React.Component {
         <h1>Robo-Trek</h1>
         {rows}
       </section>
-      <Controls handleMove={this.handleMove} />
+      <Controls handleMove={this.handleMove} handleTurn={this.handleTurn} />
       </>
     );
   };
@@ -44,7 +58,12 @@ class App extends React.Component {
 
   function Row(props) {
     const cells = Array(count).fill(null).map((item, i) =>
-      <Cell key={`row-${props.num}-cell-${i + 1}`} num={i + 1} row={props.row} col={props.row && props.col === i + 1 ? props.col : false} />
+      <Cell
+        key={`row-${props.num}-cell-${i + 1}`}
+        num={i + 1}
+        row={props.row}
+        col={props.row && props.col === i + 1 ? props.col : false}
+      />
     );
     return (
       <div className={`row-${props.num}`}>
@@ -65,11 +84,11 @@ class App extends React.Component {
   function Controls(props) {
     return (
       <div className="controls">
-        <button id="up" onClick={() => props.handleMove([-1, 0])}>Up</button>
-        <button id="down" onClick={() => props.handleMove([1, 0])}>Down</button>
-        <button id="left" onClick={() => props.handleMove([0, -1])}>Left</button>
-        <button id="right" onClick={() => props.handleMove([0, 1])}>Right</button>
-        <button id="forward">Accelerate</button>
+        <button type="button" id="north" onClick={() => props.handleTurn([-1, 0])}>North</button>
+        <button type="button" id="south" onClick={() => props.handleTurn([1, 0])}>South</button>
+        <button type="button" id="west" onClick={() => props.handleTurn([0, -1])}>West</button>
+        <button type="button" id="east" onClick={() => props.handleTurn([0, 1])}>East</button>
+        <button type="button" id="forward" onClick={() => props.handleMove()}>Accelerate</button>
       </div>
     );
   };
